9c9b71afb100565268cc93e9d9cac1c5b4d99789
h.kawai <hironori86@gmail.com>
2020-02-26 20:36:19 +0900

    photoEdit画面CSS実装済み

--- app/resources/views/action/photoEdit.blade.php
+++ app/resources/views/action/photoEdit.blade.php
@@ -17,10 +17,21 @@
             </div>
 
             <label>タイトル
-            <input type="text" class="p-profEdit__input" value="{{ old('title') }}" name="title" placeholder="タイトル" autofocus required>
+            <input type="text" class="p-photoEdit__input" value="{{ old('title') }}" name="title" placeholder="タイトル" autofocus required>
             </label>
 
-            <label>本文</label>
+            <label>本文
+            <textarea class="p-photoEdit__txt" value="{{ old('detail')}}" name="detail"></textarea>
+            </label>
+
+            @for($i = 1; $i <= 10; $i++)
+            <label>
+                タグ{{$i}}
+            <input type="text" name="tag{{$i}}" class="p-photoEdit__input" value="{{ old('tag'.$i)}}">
+            </label>
+            @endfor
+
+            <input type="submit" class="p-photoEdit__btn" value="保存">
         </form>
     </article>
 </div>
