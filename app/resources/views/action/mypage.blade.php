0a0446014485abf104786a0298b576bd2fe725fc
h.kawai <hironori86@gmail.com>
2020-04-12 20:41:34 +0900

    PC向けにレスポンシブ 対応(ログイン・登録・パスワード変更は未対応)

--- app/resources/views/action/mypage.blade.php
+++ app/resources/views/action/mypage.blade.php
@@ -9,45 +9,50 @@
 
 <main class="l-main">
 
-        <div class="p-mypage">
+    <div class="p-mypage">
 
-            <div class="p-mypage__info">
-                <div class="p-mypage__avator">
-                    <img class="u-avator--radius"
-                        src="@if($is_pic)/storage/images/profile/{{$user->pic}} @else {{asset('images/profile/person.jpeg') }} @endif"
-                        alt="">
-                </div>
-                <div class="p-mypage__description">
-                    <div class="p-mypage__name">{{ $user->name }}</div>
-                    <div class="p-mypage__comment">{{ $user->profile }}</div>
-                </div>
+        <div class="p-mypage__info">
+            <div class="p-mypage__avator">
+                <img class="u-avator--radius"
+                    src="@if($is_pic)/storage/images/profile/{{$user->pic}} @else {{asset('images/profile/person.jpeg') }} @endif"
+                    alt="">
             </div>
+            <div class="p-mypage__description">
+                <div class="p-mypage__name">{{ $user->name }}</div>
+                <div class="p-mypage__comment">{{ $user->profile }}</div>
+            </div>
+        </div>
 
-            <a href="{{ route('photoEdit')}}">
+        <div class="p-mypage__btn-list">
+
+            <a href="{{ route('photoEdit')}}" class="p-mypage__link">
                 <button class="p-mypage__btn">写真を投稿する</button>
             </a>
 
-            <a href="{{ route('profEdit')}}">
+            <a href="{{ route('profEdit')}}" class="p-mypage__link">
                 <button class="p-mypage__btn">プロフィールを編集する</button>
             </a>
 
-            <a href=" {{ route('passEdit')}}">
+            <a href=" {{ route('passEdit')}}" class="p-mypage__link">
                 <button class="p-mypage__btn">パスワードを変更する</button>
             </a>
-            <form action="{{ route('userDelete') }}" method="POST">
+            <form action="{{ route('userDelete') }}" method="POST" class="p-mypage__link">
                 @csrf
                 <button class="p-mypage__btn u-btn--alert"
                     onclick='return confirm("一度実行するとこの操作は取り消せません。本当にアカウントを削除しますか?");'>退会する</button>
             </form>
 
+        </div>
+
+
         <h1>投稿済みの写真</h1>
-        
+
         <div class="c-panels">
 
             @foreach ($pictures as $picture)
 
             <article class="c-panels__item">
-            <a href="{{ route('detail', $picture->id)}}" class="">
+                <a href="{{ route('detail', $picture->id)}}" class="">
                     <div class="c-panels__head">
                         <img src="/storage/images/photo/{{$picture->pic }}" alt="{{ $picture->title }}">
                     </div>
@@ -74,18 +79,19 @@
                     <button class="c-panels__btn ">編集</button>
                 </a>
 
-            <form method="POST" action="{{ route('deletePhoto', $picture->id )}}" class="u-inlineBlock">
+                <form method="POST" action="{{ route('deletePhoto', $picture->id )}}" class="u-inlineBlock">
                     @csrf
-                    <button class="c-panels__btn c-panels__btn--alert" onclick='return confirm("一度削除するとこの操作は取り消せません。本当に削除しますか?"); '>削除</button>
+                    <button class="c-panels__btn c-panels__btn--alert"
+                        onclick='return confirm("一度削除するとこの操作は取り消せません。本当に削除しますか?"); '>削除</button>
                 </form>
 
             </article>
 
             @endforeach
 
-            
+
         </div>
-        
+
         {{ $pictures->links('pagination::default') }}
 
     </div>
